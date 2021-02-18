f006dcd4572cf463668595e1b6329131ddc00a14
sgilyin <public.sgilyin@gmail.com>
2021-02-18 11:00:31 +0500

    Порядок полей

--- template-ctvcontrol.php
+++ template-ctvcontrol.php
@@ -30,19 +30,19 @@ if ($_POST){
     $bgbData = BGB::getData($param);
     header( "Content-Type: application/vnd.ms-excel" );
     header( "Content-disposition: attachment; filename=".date('Y-m-d')."-$param->street_id-$param->textHouse-$param->textDebt.xls" );
-    echo iconv('UTF-8', 'windows-1251', 'Адрес') . "\t" .
+    echo iconv('UTF-8', 'windows-1251', 'ФИО') . "\t" .
         iconv('UTF-8', 'windows-1251', 'Телефон') . "\t" .
-        iconv('UTF-8', 'windows-1251', 'ФИО') . "\t" .
-        iconv('UTF-8', 'windows-1251', 'Баланс') . "\t" .
+        iconv('UTF-8', 'windows-1251', 'Адрес') . "\t" .
         iconv('UTF-8', 'windows-1251', 'Тариф') . "\t" .
+        iconv('UTF-8', 'windows-1251', 'Баланс') . "\t" .
         iconv('UTF-8', 'windows-1251', 'Комментарий') . "\n";
         for ($i =0; $i < $bgbData->num_rows; $i++){
             $row = $bgbData->fetch_object();
-            echo iconv('UTF-8', 'windows-1251', $row->address) . "\t" .
+            echo iconv('UTF-8', 'windows-1251', $row->fio) . "\t" .
                 $row->phone . "\t" .
-                iconv('UTF-8', 'windows-1251', $row->fio) . "\t" .
-                $row->balance . "\t" .
+                iconv('UTF-8', 'windows-1251', $row->address) . "\t" .
                 iconv('UTF-8', 'windows-1251', $row->tariff) . "\t" .
+                $row->balance . "\t" .
                 iconv('UTF-8', 'windows-1251', $row->comment) . "\n";
         }
 } else {
